76d93760e0d7be0d8daf87e17aa476bb8ad12916
dmachlanski <damian.machlanski@gmail.com>
2022-01-24 20:46:53 +0000

    Upload CI project datasets and necessary metrics.

---
+++ project/metrics.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+def rmse(a, b):
+    """"
+    Root mean squared error between two arrays
+    """
+    return np.sqrt(np.mean((a - b)**2))
+
+def pehe(effect_true, effect_pred):
+    """
+    Precision in Estimating the Heterogeneous Treatment Effect (PEHE)
+
+    :param effect_true: true treatment effect value
+    :param effect_pred: predicted treatment effect value
+
+    :return: PEHE
+    """
+    return rmse(effect_true, effect_pred)
+
+def abs_ate(effect_true, effect_pred):
+    """
+    Absolute error for the Average Treatment Effect (ATE)
+
+    :param effect_true: true treatment effect value
+    :param effect_pred: predicted treatment effect value
+
+    :return: absolute error on ATE
+    """
+    return np.abs(np.mean(effect_pred) - np.mean(effect_true))
+
+def abs_att(effect_pred, yf, t, e):
+    """
+    Absolute error for the Average Treatment Effect on the Treated
+
+    :param effect_pred: predicted treatment effect value
+    :param yf: factual (observed) outcome
+    :param t: treatment status (treated/control)
+    :param e: whether belongs to the experimental group
+
+    :return: absolute error on ATT
+    """
+    att_true = np.mean(yf[t > 0]) - np.mean(yf[(1 - t + e) > 1])
+    att_pred = np.mean(effect_pred[(t + e) > 1])
+
+    return np.abs(att_pred - att_true)
+
+def policy_risk(effect_pred, yf, t, e):
+    """
+    Computes the risk of the policy defined by predicted effect
+
+    :param effect_pred: predicted treatment effect value
+    :param yf: factual (observed) outcome
+    :param t: treatment status (treated/control)
+    :param e: whether belongs to the experimental group
+
+    :return: policy risk
+
+    """
+    # Consider only the cases for which we have experimental data (i.e., e > 0)
+    t_e = t[e > 0]
+    yf_e = yf[e > 0]
+    effect_pred_e = effect_pred[e > 0]
+
+    if np.any(np.isnan(effect_pred_e)):
+        return np.nan
+
+    policy = effect_pred_e > 0.0
+    treat_overlap = (policy == t_e) * (t_e > 0)
+    control_overlap = (policy == t_e) * (t_e < 1)
+
+    if np.sum(treat_overlap) == 0:
+        treat_value = 0
+    else:
+        treat_value = np.mean(yf_e[treat_overlap])
+
+    if np.sum(control_overlap) == 0:
+        control_value = 0
+    else:
+        control_value = np.mean(yf_e[control_overlap])
+
+    pit = np.mean(policy)
+    policy_value = pit * treat_value + (1.0 - pit) * control_value
+
+    return 1.0 - policy_value
